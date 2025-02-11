06587602bb63f829c584282e72a9da9deb52305e
danielcollis <daniel.collis13@gmail.com>
2025-02-11 11:11:42 -0500

    initial commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Daniel Collis on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RockPaperScissorsTests/RockPaperScissorsTests.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsTests.swift
+//  RockPaperScissorsTests
+//
+//  Created by Daniel Collis on 2/11/25.
+//
+
+import Testing
+@testable import RockPaperScissors
+
+struct RockPaperScissorsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
