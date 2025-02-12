3c3eb9963d5691cf346b0306f2d04446402f5131
danielcollis <daniel.collis13@gmail.com>
2025-02-12 10:55:33 -0500

    basic logic

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,14 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    let moves = ["Rock", "Paper", "Scissors"]
+    let whatBeatsMoves = ["Paper", "Scissors", "Rock"]
+    let whatLosesToMoves = ["Scissors", "Rock", "Paper"]
+    @State private var cpuMove = Int.random(in: 0..<3)
+    @State private var winOrLose = Bool.random()
+    
+    @State private var playerScore = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack {
+                Text("Score: \(playerScore)")
+                
+                Text("Computer Move: \(moves[cpuMove])")
+                
+                Text((winOrLose) ? "What wins against \(moves[cpuMove])" : "What loses against \(moves[cpuMove])?")
+                
+                HStack {
+                    ForEach(moves, id: \.self) { move in
+                        Button {
+                            if isAnswerCorrect(move) {
+                                print("Correct")
+                            } else {
+                                print("Incorrect")
+                            }
+                            winOrLose.toggle()
+                            cpuMove = Int.random(in: 0..<3)
+                        } label : {
+                            Text(displayEmoji(for: move))
+                                .font(.largeTitle)
+                        }
+                    }
+                }
+            }
         }
-        .padding()
+    }
+    
+    func isAnswerCorrect(_ answer: String) -> Bool {
+        switch winOrLose {
+        case true:
+            return answer == whatBeatsMoves[cpuMove]
+        case false:
+            return answer == whatLosesToMoves[cpuMove]
+        }
+    }
+    
+    func displayEmoji (for move: String) -> String {
+        switch move {
+        case "Rock":
+            return "🪨"
+        case "Paper":
+            return "📄"
+        case "Scissors":
+            return "✂️"
+        default:
+            return "None"
+        }
     }
 }
 
